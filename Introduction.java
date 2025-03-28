a873e39104a5cdb4f266561d6b9b61f48297d367
ndolocr <crndolo@gmail.com>
2025-03-28 08:52:02 +0300

    Project initialization.

---
+++ Introduction.java
@@ -0,0 +1,28 @@
+public class Introduction {
+    public static void main(String[] args) {
+        String data = """
+        This is a program that introduces us to data Structures and Algorithms.
+        
+        DATA STRUCTURES
+        A Data structure is a way to store multiple values in a single structure. 
+        Data structures are categorised as:
+        
+        1. Linear
+            - Array
+            - List
+            - Stack
+            - Queues
+        2. Non-Linear
+            - Tree
+            - Graph
+       
+       ALGORITHM
+       An algorithm is a method that is used to perform a specific task. Although there might
+       be many different ways to perform a specific task, the most important algorithm is one
+       that keeps in mind both:
+            - Time Complexity
+            - Space Complexity. 
+        """;
+        System.out.println(data);
+    }
+}
---
+++ out/production/dataStructuresAndAlgorithimsInJAVA/README.md
@@ -0,0 +1,2 @@
+# dataStructuresAndAlgorithimsInJAVA
+This repo is used to have all my learning practice of data Structures and algorithims
